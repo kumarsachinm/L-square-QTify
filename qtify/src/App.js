54a1fa8d05058674f61c2910d82bc6e27431d2f5
Sachin Kumar <kumarsachinm@gmail.com>
2024-05-01 08:43:05 +0530

    navbar

---
+++ qtify/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css';
+import "./Components/Navbar/Navbar"
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      </>
+  );
+}
+
+export default App;
